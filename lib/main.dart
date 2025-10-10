4aa1b5434bae177341d327944a529953ece181e0
Rainbow0315 <Maureen0315@163.com>
2025-10-10 14:42:21 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,8 @@ class PandoraApp extends StatelessWidget {
           background: Color(0xFFFFF8E1),
         ),
       ),
-      home: HomeScreen(),
+      // home: HomeScreen(),
+      home: LoginScreen(),
       routes: {
         '/home': (context) => HomeScreen(),
         '/tasks': (context) => TaskScreen(),
