107ca14a69bc9aa83d5527f10fa328806a3f3932
Rainbow0315 <Maureen0315@163.com>
2025-10-10 15:02:40 +0800

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  static const String baseUrl = "http://127.0.0.1:8080/api";
+  static const String baseUrl = "http://127.0.0.1:8000/api";
 
   static Future<String?> login(String email, String password) async {
     final url = Uri.parse("$baseUrl/login/");
